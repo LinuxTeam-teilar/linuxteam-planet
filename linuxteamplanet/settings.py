28739b895025464322398a6fb18b9e461b39cc45
Giorgos Tsiapaliokas <terietor@gmail.com>
2012-09-18 19:50:15 +0300

    Initial commit for templates

--- linuxteamplanet/settings.py
+++ linuxteamplanet/settings.py
@@ -134,6 +134,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+
+    ROOT_PROJECT + '/linuxteamplanet/templates/'
 )
 
 INSTALLED_APPS = (
