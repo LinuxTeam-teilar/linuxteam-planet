67e4b8ad26891bd9e00646e43cb91ba7189f9799
Giorgos Tsiapaliokas <terietor@gmail.com>
2012-09-19 21:31:57 +0300

    Remove duplication

--- linuxteamplanet/posts/views.py
+++ linuxteamplanet/posts/views.py
@@ -18,7 +18,7 @@
 from django.shortcuts import render_to_response
 
 def home(request):
-    return render_to_response('home.html', {})
+    return render_to_response('posts.html', {})
 
 def posts_id(resquest, post_id):
-    return render_to_response('post.html', {})
+    return render_to_response('posts.html', {})
