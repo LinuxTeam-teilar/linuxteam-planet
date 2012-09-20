6250909434bc46fca8b6ca1d28d7203c80585fcc
Giorgos Tsiapaliokas <terietor@gmail.com>
2012-09-20 14:52:17 +0300

    add logger

--- linuxteamplanet/settings.py
+++ linuxteamplanet/settings.py
@@ -160,6 +160,14 @@ INSTALLED_APPS = (
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s: %(name)s: %(message)s'
+        }
+    },
     'filters': {
         'require_debug_false': {
             '()': 'django.utils.log.RequireDebugFalse'
@@ -170,6 +178,11 @@ LOGGING = {
             'level': 'ERROR',
             'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
         }
     },
     'loggers': {
@@ -177,6 +190,10 @@ LOGGING = {
             'handlers': ['mail_admins'],
             'level': 'ERROR',
             'propagate': True,
+        },
+        'linuxteamplanet': {
+            "handlers": ["console"],
+            "level": "DEBUG"
         },
     }
 }
